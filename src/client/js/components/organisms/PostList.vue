6ba47f9c5e2205cb122880bae060808b1b9e4678
Shingo Yamada <uzuranoie@gmail.com>
2022-01-28 11:37:43 +0900

    Implemented posts component for commonization

---
+++ src/client/js/components/organisms/PostList.vue
@@ -0,0 +1,111 @@
+<template>
+<div>
+  <ul v-if="posts">
+    <li
+      v-for="post in posts"
+      class="block"
+    >
+      <div class="card">
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <h4 class="title is-4">
+                <router-link
+                  :to="`/posts/${serviceId}/${post.slug}`"
+                >{{ post.title }}</router-link>
+              </h4>
+            </div>
+          </div>
+
+          <div class="content">
+            {{ post.body }}
+            <br>
+            <time
+             itemprop="datepublished"
+              :datetime="post.publishAt | dateFormat('')"
+            >{{ post.publisheeAt | dateFormat }}</time>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
+
+  <nav v-if="hasNext" class="u-mt2r">
+    <a class="u-clickable" @click="fetchPosts({ untilTime:lastItemPublishedAt })">{{ $t('common.more') }}</a>
+  </nav>
+</div>
+</template>
+<script>
+import moment from '@/moment'
+import { Post } from '@/api'
+
+export default{
+  props: {
+  },
+
+  data(){
+    return {
+      posts: [],
+      hasNext: false,
+      listCount: 3,
+    }
+  },
+
+  computed: {
+    serviceId() {
+      return this.$route.params.serviceId
+    },
+
+    categorySlug() {
+      return this.$route.params.categorySlug
+    },
+
+    lastItemPublishedAt () {
+      const lastIndex = this.posts.length - 1
+      return this.posts.length > 0 ? encodeURI(this.posts[lastIndex].publishAt) : null
+    },
+  },
+
+  async created() {
+    await this.fetchPosts()
+  },
+
+  methods: {
+    async fetchPosts(params = {}, isLatest = false) {
+      const params_copied = { ...params }
+      params_copied.count = this.listCount + 1
+      if (this.categorySlug) {
+        params_copied.category = this.categorySlug
+      }
+      //this.listLoaded = false
+      this.$store.dispatch('setLoading', true)
+      try {
+        let items = await Post.get(this.serviceId, null, params_copied)
+        if (isLatest) {
+          items.reverse()
+        } else {
+          if (items.length > this.listCount) {
+            items.pop()
+            this.hasNext = true
+          } else {
+            this.hasNext = false
+          }
+        }
+        items.map(item => {
+          if (isLatest) {
+            this.posts.unshift(item)
+          } else {
+            this.posts.push(item)
+          }
+        })
+        //this.listLoaded = true
+        this.$store.dispatch('setLoading', false)
+      } catch (err) {
+        this.$store.dispatch('setLoading', false)
+        this.handleApiError(err, this.$t('msg["Failed to get data from server"]'))
+      }
+    },
+  },
+}
+</script>
+
--- src/client/js/templates/Posts.vue
+++ src/client/js/templates/Posts.vue
@@ -1,55 +1,24 @@
 <template>
 <div>
   <div>
-    <h1 class="title">Posts of {{ serviceId }}</h1>
+    <h1 class="title">{{ $t('common.posts') }}</h1>
+    <p class="subtitle is-5">ServiceID: {{ serviceId }}</p>
   </div>
-  <ul v-if="posts">
-    <li
-      v-for="post in posts"
-      class="block"
-    >
-      <div class="card">
-        <div class="card-content">
-          <div class="media">
-            <div class="media-content">
-              <h4 class="title is-4">
-                <router-link
-                  :to="`/posts/${serviceId}/${post.slug}`"
-                >{{ post.title }}</router-link>
-              </h4>
-            </div>
-          </div>
-
-          <div class="content">
-            hogehoge
-            <br>
-            <time
-             itemprop="datepublished"
-              :datetime="post.publishAt | dateFormat('')"
-            >{{ post.publisheeAt | dateFormat }}</time>
-          </div>
-        </div>
-      </div>
-    </li>
-  </ul>
-
-  <nav v-if="hasNext" class="u-mt2r">
-    <a class="u-clickable" @click="fetchPosts({ untilTime:lastItemPublishedAt })">{{ $t('common.more') }}</a>
-  </nav>
+  <post-list class="mt-6"></post-list>
 </div>
 </template>
 <script>
-import moment from '@/moment'
-import { Post } from '@/api'
+import PostList from '@/components/organisms/PostList'
 
 export default{
   name: 'Posts',
 
-  data(){
+  components: {
+    PostList,
+  },
+
+  data() {
     return {
-      posts: [],
-      hasNext: false,
-      listCount: 3,
     }
   },
 
@@ -57,55 +26,12 @@ export default{
     serviceId() {
       return this.$route.params.serviceId
     },
-
-    lastItemPublishedAt () {
-      const lastIndex = this.posts.length - 1
-      return this.posts.length > 0 ? encodeURI(this.posts[lastIndex].publishAt) : null
-      //return this.posts.length > 0 ? encodeURI(moment(this.posts[lastIndex].publishAt).toISOString()) : null
-    },
   },
 
   created() {
-    this.fetchPosts()
   },
 
   methods: {
-    async fetchPosts(params = {}, isLatest = false) {
-      const params_copied = { ...params }
-      params_copied.count = this.listCount + 1
-      //this.listLoaded = false
-      this.$store.dispatch('setLoading', true)
-      try {
-        let items
-        if (this.isProducerPage) {
-          items = await Post.get(this.serviceId, null, params_copied)
-        } else {
-          items = await Post.get(this.serviceId, null, params_copied)
-        }
-        if (isLatest) {
-          items.reverse()
-        } else {
-          if (items.length > this.listCount) {
-            items.pop()
-            this.hasNext = true
-          } else {
-            this.hasNext = false
-          }
-        }
-        items.map(item => {
-          if (isLatest) {
-            this.posts.unshift(item)
-          } else {
-            this.posts.push(item)
-          }
-        })
-        //this.listLoaded = true
-        this.$store.dispatch('setLoading', false)
-      } catch (err) {
-        this.$store.dispatch('setLoading', false)
-        this.handleApiError(err, this.$t('msg["Failed to get data from server"]'))
-      }
-    },
   },
 }
 </script>
